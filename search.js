f2d30582e8c8f8fb3b30d551752ed7de59b960c2
Kal <kaumil.patel@ucalgary.ca>
2021-05-20 22:07:35 -0600

    fix

--- search.js
+++ search.js
@@ -4,43 +4,40 @@ for(let i=0;i<100;i++){
 }
 arr.forEach(index =>  document.getElementById('array').innerHTML += '<div class="grid-item" id="arr '+index+'">'+index+'</div>');
 
-var comparisons;
-
 function binarySearch(){
-    comparisons = 0;
+    var comparisons = 0;
     document.getElementById('array').innerHTML = "";
     arr.forEach(index =>  document.getElementById('array').innerHTML += '<div class="grid-item" id="arr '+index+'">'+index+'</div>');
     let target = document.getElementById('target').value;
-    binarySearchRecursive(target, 0, arr.length-1);
+    binarySearchRecursive(target, 0, arr.length-1, comparisons);
 }
 
-async function binarySearchRecursive(target, low, high){
+async function binarySearchRecursive(target, low, high, comparisons){
+    document.getElementById('binaryComparison').innerHTML = comparisons;
     if(high<low){
-        document.getElementById('binaryComparison').innerHTML = comparisons;
         return -1;
     }
     let mid = Math.floor(low + (high-low) / 2);
     if(arr[mid]==target){
         document.getElementById("arr "+mid).style.backgroundColor = "green";
-        document.getElementById('binaryComparison').innerHTML = comparisons;
         return mid;
     }else if(target < arr[mid]){
         comparisons++;
         document.getElementById("arr "+mid).innerHTML = "<";
         document.getElementById("arr "+mid).style.backgroundColor = "yellow";
         await sleep(250);
-        binarySearchRecursive(target, low, mid-1);
+        binarySearchRecursive(target, low, mid-1, comparisons);
     } else{
         comparisons++;
         document.getElementById("arr "+mid).innerHTML = ">";
         document.getElementById("arr "+mid).style.backgroundColor = "red";
         await sleep(250);
-        binarySearchRecursive(target, mid+1, high);
+        binarySearchRecursive(target, mid+1, high, comparisons);
     }
 }
 
 async function linearSearch(){
-    comparisons = 0;
+    var comparisons = 0;
     document.getElementById('array').innerHTML = "";
     arr.forEach(index =>  document.getElementById('array').innerHTML += '<div class="grid-item" id="arr '+index+'">'+index+'</div>');
     let target = document.getElementById('target').value;
